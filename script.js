50d82a07c42fa14c436f0c7652f8562dc8f737d4
Kamal Deep <86801232+Deepgautam76@users.noreply.github.com>
2025-06-07 09:58:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 //complete this code
-class Person {}
+class Person {
+	cunstructor(name,age){
+		this.name=name;
+		this.age=age;
+   }
+	study(){
+		// console.log(this.name +" is studying");
+    }
+	teach(){
+		// console.log(this.name+" is teaching");
+   }
+   get name(){
+	   return this.name;
+   }	
+   set age(age){
+	   this.age=age;
+   }	
+}
 
-class Student extends Person {}
+class Student extends Person {
+	custructor(name,age){
+		super(name,age);
+   }
+	study(){
+		console.log(this.name+" is studying");
+   }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	custructor(name,age){
+		super(name,age);
+    }
+	teach(){
+		console.log(this.name+" is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
