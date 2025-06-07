11f1b2915efe507c9bea3f40e191f6bb106d7a32
Kamal Deep <86801232+Deepgautam76@users.noreply.github.com>
2025-06-07 10:09:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,14 +5,20 @@ class Person {
 		this.age=age;
    }
 	study(){
-		// console.log(this.name +" is studying");
+		console.log(this.name +" is studying");
     }
 	teach(){
-		// console.log(this.name+" is teaching");
+		console.log(this.name+" is teaching");
    }
    get name(){
 	   return this.name;
    }	
+	set name(newName){
+		this.name=newName;
+   }
+	get age(){
+		return this.age;
+   }
    set age(age){
 	   this.age=age;
    }	
@@ -35,6 +41,14 @@ class Teacher extends Person {
 		console.log(this.name+" is teaching");
 	}
 }
+Person person=new Person("John",25);
+console.log(person.name);
+person.age(30);
+console.log(person.study());
+
+Teacher teacher=new Teacher("John",30);
+console.log(teacher.teach());
+
 
 // Do not change the code below this line
 window.Person = Person;
